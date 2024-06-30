2f653a35661f25aa30884b45c2f63b22c600c986
Nikitha Muruleedhar <nmuruleedhar@hexonglobal.com>
2024-06-30 17:15:04 +0530

    icon images added for the todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import API from "../utils/API";
-import Plus from "../assets/Plus.png"
-
+import Plus from "../assets/Plus.png";
+import DeleteIcon from "../assets/DeleteIcon.png";
+import EditIcon from "../assets/EditIcon.png";
+import SaveIcon from "../assets/SaveIcon.png";
 const Todo = () => {
   const [tasks, setTasks] = useState("");
   const [taskList, setTaskList] = useState([]);
@@ -54,74 +56,72 @@ const Todo = () => {
   };
 
   return (
-   <div className="flex h-screen w-full justify-center items-center bg-[#302f2f]">
-  <div className="flex flex-col justify-center items-center p-4 border border-white rounded-lg">
-    <h1 className="text-2xl  text-white text-center font-bold mb-4">To Do List</h1>
-    <div className="flex flex-row justify-center items-center gap-2 mb-4">
-      <input
-        type="text"
-        placeholder="Add a new task"
-        onChange={(e) => setTasks(e.target.value)}
-        value={tasks}
-        className="px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-black"
-      />
-      <button
-        onClick={handleAddTask}
-        className="bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600 focus:ring w-10 h-10 flex items-center justify-center"
-
-      >
-        <img src={Plus} alt="Add" className="w-6 h-6" />
-      
-      </button>
-      <button
-        onClick={() => setTasks("")}
-        className="bg-gray-300 text-gray-800 px-6 py-2 rounded-full hover:bg-gray-400 "
-      >
-        Clear
-      </button>
+    <div className="flex  flex-col h-screen w-screen justify-center items-center bg-[#302f2f]">
+      <h1 className="text-2xl  text-white text-center font-bold mb-4">
+        To Do List
+      </h1>
+      <div className="flex flex-row justify-center items-center gap-2 mb-4">
+        <input
+          type="text"
+          placeholder="Add a new task"
+          onChange={(e) => setTasks(e.target.value)}
+          value={tasks}
+          className="px-4 py-2 border w-full rounded-full focus:outline-none focus:ring-2 focus:ring-black"
+        />
+        <button
+          onClick={handleAddTask}
+          className="bg-[#f0f0f0] p-2 rounded-full w-20 h-10 flex items-center justify-center"
+        >
+          <img src={Plus} alt="Add" className="w-6 h-6" />
+        </button>
+        <button
+          onClick={() => setTasks("")}
+          className="bg-gray-300 text-gray-800 px-6 py-2 rounded-full hover:bg-gray-400 "
+        >
+          Clear
+        </button>
+      </div>
+      <ul className="list-none">
+        {taskList.map((task, index) => (
+          <li key={task._id} className="flex items-center gap-2 mb-2">
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editedTask}
+                  onChange={(e) => setEditedTask(e.target.value)}
+                  className="px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <button
+                  onClick={() => handleSave(index)}
+                  className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600"
+                >
+                  <img src={SaveIcon} alt="Save" className="w-6 h-6" />
+                </button>
+              </>
+            ) : (
+              <>
+                <span className="px-4 py-2 w-full text-white border rounded-2xl flex">
+                  {task.task}
+                </span>
+                <button
+                  onClick={() => handleEdit(index)}
+                  className="bg-yellow-500 p-2 rounded-full hover:bg-yellow-600 w-full h-full flex justify-center items-center"
+                >
+                  <img src={EditIcon} alt="Edit" className="w-6 h-6" />
+                </button>
+                <button
+                  onClick={() => handleDelete(index)}
+                  className="bg-red-500 w-full h-full  p-2 rounded-full hover:bg-red-600 flex items-center justify-center"
+                >
+                  <img src={DeleteIcon} alt="Delete" className="w-6 h-6" />
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
-    <ul className="list-none">
-      {taskList.map((task, index) => (
-        <li key={task._id} className="flex items-center gap-2 mb-2">
-          {editIndex === index ? (
-            <>
-              <input
-                type="text"
-                value={editedTask}
-                onChange={(e) => setEditedTask(e.target.value)}
-                className="px-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-              <button
-                onClick={() => handleSave(index)}
-                className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600"
-              >
-                Save
-              </button>
-            </>
-          ) : (
-            <>
-              <span className="px-4 py-2 text-white w-full border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500">
-                {task.task}
-              </span>
-              <button
-                onClick={() => handleEdit(index)}
-                className="bg-yellow-500 text-white px-6 py-2 rounded-full hover:bg-yellow-600"
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => handleDelete(index)}
-                className="bg-red-500 text-white px-6 py-2 rounded-full hover:bg-red-600"
-              >
-                Delete
-              </button>
-            </>
-          )}
-        </li>
-      ))}
-    </ul>
-  </div>
-</div>
   );
 };
 
